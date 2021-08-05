1a7e8905afb8d4dcbcac26f5315417dde3bad7b1
Joker <2028318192@qq.com>
2021-08-05 22:27:14 +0800

    redis工具类

--- foodie-dev-api/src/main/java/com/joker/controller/RedisController.java
+++ foodie-dev-api/src/main/java/com/joker/controller/RedisController.java
@@ -1,5 +1,6 @@
 package com.joker.controller;
 
+import com.joker.utils.RedisOperator;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,23 +21,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class RedisController {
 
     private final RedisTemplate redisTemplate;
+    private final RedisOperator redisOperator;
 
-    public RedisController(RedisTemplate redisTemplate) {
+    public RedisController(RedisTemplate redisTemplate, RedisOperator redisOperator) {
         this.redisTemplate = redisTemplate;
+        this.redisOperator = redisOperator;
     }
 
     @PostMapping("/set")
     public Object set(@RequestParam String key, @RequestParam String value){
-        redisTemplate.opsForValue().set(key,value);
+//        redisTemplate.opsForValue().set(key,value);
+        redisOperator.set(key, value);
         return "set ok";
     }
     @PostMapping("/get")
     public Object get(@RequestParam String key){
-        return (String)redisTemplate.opsForValue().get(key);
+//        return (String)redisTemplate.opsForValue().get(key);
+        return redisOperator.get(key);
     }
     @PostMapping("/del")
     public Object del(@RequestParam String key){
-        Boolean delete = redisTemplate.delete(key);
+//        Boolean delete = redisTemplate.delete(key);
+        redisOperator.del(key);
         return "del ok";
     }
 }
